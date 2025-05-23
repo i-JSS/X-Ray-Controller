28e1d98782fa5649ccfd2ec3354d4515030e2211
jss <joaoseisei@gmail.com>
2025-05-23 00:17:49 -0300

    Adiciona gpio

---
+++ src/gpio.h
@@ -0,0 +1,30 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#define MOTOR_X_PWM 17
+#define MOTOR_X_DIR1 27
+#define MOTOR_X_DIR2 22
+#define MOTOR_Y_PWM 23
+#define MOTOR_Y_DIR1 24
+#define MOTOR_Y_DIR2 25
+
+#define ENCODER_X_A 5
+#define ENCODER_X_B 6
+#define ENCODER_Y_A 12
+#define ENCODER_Y_B 13
+
+#define SENSOR_X_MIN 26
+#define SENSOR_X_MAX 19
+#define SENSOR_Y_MIN 20
+#define SENSOR_Y_MAX 21
+
+#define CAPTURA 18
+
+#define BOTAO_CIMA 16
+#define BOTAO_BAIXO 1
+#define BOTAO_ESQ 6
+#define BOTAO_DIR 8
+
+#define BOTAO_EMERGENCIA 11
+
+#endif //GPIO_H
