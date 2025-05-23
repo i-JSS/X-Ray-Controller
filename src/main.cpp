afceaff76890b3f2fcc310ed254a688826864956
Hunter104 <000plagueinc@gmail.com>
2025-05-22 21:19:19 -0300

    Adiciona mock de função main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
+#include "uart.h"
+#include <atomic>
+#include <csignal>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello, world!\n";
+std::atomic<bool> running{true};
+void handler(int) { running.store(false); }
+
+int main(void) {
+  struct sigaction sa;
+  sa.sa_handler = handler;
+  sigfillset(&sa.sa_mask);
+  sigaction(SIGINT, &sa, nullptr);
+
+  // Loop principal
+  UARTController uart;
+  while (true) {
+
+    if (!running.load())
+      break;
+  }
+
+  // Destrutores cuidam de fechar e parar o sistema graciosamente
   return 0;
 }
