98330333043647a478ee3cb3d09c1efee41d0e31
Hunter104 <000plagueinc@gmail.com>
2025-05-23 22:03:18 -0300

    Move file descriptor para atributo

--- src/Uart.cpp
+++ src/Uart.cpp
@@ -2,206 +2,208 @@
 #include <cstdint>
 #include <fcntl.h>
 #include <iostream>
-#include <utility>
+#include <string>
 #include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 #include <vector>
-#include <iomanip>
-#include <string>
 
 using namespace std;
 
 class Uart {
 public:
-    int fd; // ta aqui pra quando der ctrl c podemos fechar com segurança a qualquer momento!
-    uint8_t ESP_ADDRESS = 0x01;
-    array<uint8_t, 4> MATRICULA = {8, 1, 5, 0};
+  int fd;
+  uint8_t ESP_ADDRESS = 0x01;
+  array<uint8_t, 4> MATRICULA = {8, 1, 5, 0};
 
-    enum class Code : uint8_t {
-        READ = 0x03,
-        WRITE = 0x06,
+  enum class Code : uint8_t {
+    READ = 0x03,
+    WRITE = 0x06,
+  };
+  enum SubCode : uint8_t {
+    MOVE_X_LEFT_RIGHT = 0x00,
+    MOVE_Y_UP_DOWN = 0x01,
+    PRESET_POSITIONS = 0x02,
+    SET_PRESET_POSITION = 0x03,
+    CALIBRATE = 0x04,
+    REG_SPEED_X = 0x05,
+    REG_SPEED_Y = 0x09,
+    REG_POSITION_X = 0x0D,
+    REG_POSITION_Y = 0x11,
+    REG_TEMPERATURE = 0x15,
+    REG_PRESSURE = 0x19,
+    REG_MACHINE_STATE = 0x1D
+  };
+
+  Uart(const string &portName, const speed_t baudRate)
+      : portName(portName), baudRate(baudRate) {}
+
+  vector<uint8_t> mountMessageRead(SubCode subcode) {
+    vector<uint8_t> msg = {
+        ESP_ADDRESS,
+        static_cast<uint8_t>(Code::READ),
+        static_cast<uint8_t>(subcode),
     };
-    enum SubCode : uint8_t {
-        MOVE_X_LEFT_RIGHT = 0x00,
-        MOVE_Y_UP_DOWN = 0x01,
-        PRESET_POSITIONS = 0x02,
-        SET_PRESET_POSITION = 0x03,
-        CALIBRATE = 0x04,
-        REG_SPEED_X = 0x05,
-        REG_SPEED_Y = 0x09,
-        REG_POSITION_X = 0x0D,
-        REG_POSITION_Y = 0x11,
-        REG_TEMPERATURE = 0x15,
-        REG_PRESSURE = 0x19,
-        REG_MACHINE_STATE = 0x1D
-    };
+    msg.push_back(0x01);
 
-    Uart(string portName, const speed_t baudRate)
-        : portName(move(portName)), baudRate(baudRate) {}
+    msg.insert(msg.end(), MATRICULA.begin(), MATRICULA.end());
 
-    vector<uint8_t> mountMessageRead(SubCode subcode) {
-        vector<uint8_t> msg = {
-            ESP_ADDRESS,
-            static_cast<uint8_t>(Code::READ),
-            static_cast<uint8_t>(subcode),
-        };
-        msg.push_back(0x01);
+    uint16_t crc = calculateCRC(msg.data(), msg.size());
 
-        msg.insert(msg.end(), MATRICULA.begin(), MATRICULA.end());
+    msg.push_back(crc);
+    msg.push_back((crc >> 8));
 
-        uint16_t crc = calculateCRC(msg.data(), msg.size());
+    cout << "Enviando: ";
+    printHex(msg);
+    return msg;
+  }
 
-        msg.push_back(crc);
-        msg.push_back((crc >> 8));
-
-        cout << "Enviando: ";
-        printHex(msg);
-        return msg;
-    }
-
-    void requestRead(SubCode subcode) {
-        fd = openSerial();
-        while (true) {
-            vector<uint8_t> msg = mountMessageRead(subcode);
-            if (write(fd, msg.data(), msg.size()) != static_cast<int>(msg.size())) {
-                cerr << "Erro ao enviar dados.\n";
-                close(fd);
-                return;
-            }
-
-            fsync(fd);
-            usleep(100000);
-
-            uint8_t buffer[256];
-            ssize_t len = read(fd, buffer, sizeof(buffer));
-            if (len < 0) {
-                cerr << "Erro na leitura da resposta.\n";
-                close(fd);
-                return;
-            }
-
-            vector<uint8_t> resposta(buffer, buffer + len);
-
-            if (isValidCRC(resposta.data(), resposta.size())) {
-                cout << "CRC válido! Resposta recebida: ";
-                printHex(resposta);
-                uint32_t valor = resposta[2];
-                cout << "Valor inteiro: " << valor << endl;
-                break;
-            }
-            cout << "CRC inválido. Tentando novamente...\n";
-        }
+  void requestRead(SubCode subcode) {
+    ensureOpen();
+    while (true) {
+      vector<uint8_t> msg = mountMessageRead(subcode);
+      if (write(fd, msg.data(), msg.size()) != static_cast<int>(msg.size())) {
+        cerr << "Erro ao enviar dados.\n";
         close(fd);
+        return;
+      }
+
+      fsync(fd);
+      usleep(100000);
+
+      uint8_t buffer[256];
+      ssize_t len = read(fd, buffer, sizeof(buffer));
+      if (len < 0) {
+        cerr << "Erro na leitura da resposta.\n";
+        close(fd);
+        return;
+      }
+
+      vector<uint8_t> resposta(buffer, buffer + len);
+
+      if (isValidCRC(resposta.data(), resposta.size())) {
+        cout << "CRC válido! Resposta recebida: ";
+        printHex(resposta);
+        uint32_t valor = resposta[2];
+        cout << "Valor inteiro: " << valor << endl;
+        break;
+      }
+      cout << "CRC inválido. Tentando novamente...\n";
     }
+    ensureClosed();
+  }
+
+  ~Uart() { ensureClosed(); }
 
 private:
-    string portName;
-    speed_t baudRate;
+  string portName;
+  speed_t baudRate;
 
-    int openSerial() {
-        int fd = open(portName.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
-        if (fd == -1) {
-            perror("Erro ao abrir UART");
-            return -1;
-        }
+  void ensureClosed() {
+    if (fd != -1) {
+      close(fd);
+      fd = -1;
+    }
+  }
 
-        termios tty{};
-        if (tcgetattr(fd, &tty) != 0) {
-            perror("Erro ao obter atributos da UART");
-            close(fd);
-            return -1;
-        }
+  void ensureOpen() {
+    fd = open(portName.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd == -1)
+      throw std::system_error(errno, std::generic_category(),
+                              "Erro ao abrir a porta serial");
 
-        cfsetospeed(&tty, baudRate);
-        cfsetispeed(&tty, baudRate);
-
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
-        tty.c_cflag |= (CLOCAL | CREAD);
-        tty.c_cflag &= ~(PARENB | PARODD);
-        tty.c_cflag &= ~CSTOPB;
-        tty.c_cflag &= ~CRTSCTS;
-
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-        tty.c_iflag &= ~IGNBRK;
-        tty.c_lflag = 0;
-        tty.c_oflag = 0;
-
-        tty.c_cc[VMIN]  = 1;
-        tty.c_cc[VTIME] = 1;
-
-        if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-            perror("Erro ao aplicar configurações na UART");
-            close(fd);
-            return -1;
-        }
-
-        return fd;
+    termios tty{};
+    if (tcgetattr(fd, &tty) != 0) {
+      close(fd);
+      throw std::system_error(errno, std::generic_category(),
+                              "Erro ao obter atributos da UART");
     }
 
+    cfsetospeed(&tty, baudRate);
+    cfsetispeed(&tty, baudRate);
 
-    short CRC16(short crc, char data){
-        const unsigned short tbl[256] = {
-            0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
-            0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440,
-            0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81, 0x0E40,
-            0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841,
-            0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40,
-            0x1E00, 0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41,
-            0x1400, 0xD4C1, 0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641,
-            0xD201, 0x12C0, 0x1380, 0xD341, 0x1100, 0xD1C1, 0xD081, 0x1040,
-            0xF001, 0x30C0, 0x3180, 0xF141, 0x3300, 0xF3C1, 0xF281, 0x3240,
-            0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501, 0x35C0, 0x3480, 0xF441,
-            0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0, 0x3E80, 0xFE41,
-            0xFA01, 0x3AC0, 0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881, 0x3840,
-            0x2800, 0xE8C1, 0xE981, 0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41,
-            0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1, 0xEC81, 0x2C40,
-            0xE401, 0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640,
-            0x2200, 0xE2C1, 0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041,
-            0xA001, 0x60C0, 0x6180, 0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240,
-            0x6600, 0xA6C1, 0xA781, 0x6740, 0xA501, 0x65C0, 0x6480, 0xA441,
-            0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01, 0x6FC0, 0x6E80, 0xAE41,
-            0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1, 0xA881, 0x6840,
-            0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80, 0xBA41,
-            0xBE01, 0x7EC0, 0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40,
-            0xB401, 0x74C0, 0x7580, 0xB541, 0x7700, 0xB7C1, 0xB681, 0x7640,
-            0x7200, 0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0, 0x7080, 0xB041,
-            0x5000, 0x90C1, 0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241,
-            0x9601, 0x56C0, 0x5780, 0x9741, 0x5500, 0x95C1, 0x9481, 0x5440,
-            0x9C01, 0x5CC0, 0x5D80, 0x9D41, 0x5F00, 0x9FC1, 0x9E81, 0x5E40,
-            0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901, 0x59C0, 0x5880, 0x9841,
-            0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1, 0x8A81, 0x4A40,
-            0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80, 0x8C41,
-            0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641,
-            0x8201, 0x42C0, 0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040
-        };
-        return ((crc & 0xFF00) >> 8) ^ tbl[(crc & 0x00FF) ^ (data & 0x00FF)];
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
+    tty.c_cflag |= (CLOCAL | CREAD);
+    tty.c_cflag &= ~(PARENB | PARODD);
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_iflag &= ~IGNBRK;
+    tty.c_lflag = 0;
+    tty.c_oflag = 0;
+
+    tty.c_cc[VMIN] = 1;
+    tty.c_cc[VTIME] = 1;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+      close(fd);
+      throw std::system_error(errno, std::generic_category(),
+                              "Erro ao aplicar configurações na UART");
     }
+  }
 
-    short calculateCRC(const unsigned char *commands, const int size) {
-        short crc = 0;
-        for(int i = 0;i<size;i++) crc = CRC16(crc, commands[i]);
-        return crc;
-    }
+  short CRC16(short crc, char data) {
+    const unsigned short tbl[256] = {
+        0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241, 0xC601,
+        0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440, 0xCC01, 0x0CC0,
+        0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81, 0x0E40, 0x0A00, 0xCAC1, 0xCB81,
+        0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841, 0xD801, 0x18C0, 0x1980, 0xD941,
+        0x1B00, 0xDBC1, 0xDA81, 0x1A40, 0x1E00, 0xDEC1, 0xDF81, 0x1F40, 0xDD01,
+        0x1DC0, 0x1C80, 0xDC41, 0x1400, 0xD4C1, 0xD581, 0x1540, 0xD701, 0x17C0,
+        0x1680, 0xD641, 0xD201, 0x12C0, 0x1380, 0xD341, 0x1100, 0xD1C1, 0xD081,
+        0x1040, 0xF001, 0x30C0, 0x3180, 0xF141, 0x3300, 0xF3C1, 0xF281, 0x3240,
+        0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501, 0x35C0, 0x3480, 0xF441, 0x3C00,
+        0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0, 0x3E80, 0xFE41, 0xFA01, 0x3AC0,
+        0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881, 0x3840, 0x2800, 0xE8C1, 0xE981,
+        0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41, 0xEE01, 0x2EC0, 0x2F80, 0xEF41,
+        0x2D00, 0xEDC1, 0xEC81, 0x2C40, 0xE401, 0x24C0, 0x2580, 0xE541, 0x2700,
+        0xE7C1, 0xE681, 0x2640, 0x2200, 0xE2C1, 0xE381, 0x2340, 0xE101, 0x21C0,
+        0x2080, 0xE041, 0xA001, 0x60C0, 0x6180, 0xA141, 0x6300, 0xA3C1, 0xA281,
+        0x6240, 0x6600, 0xA6C1, 0xA781, 0x6740, 0xA501, 0x65C0, 0x6480, 0xA441,
+        0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01, 0x6FC0, 0x6E80, 0xAE41, 0xAA01,
+        0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1, 0xA881, 0x6840, 0x7800, 0xB8C1,
+        0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80, 0xBA41, 0xBE01, 0x7EC0, 0x7F80,
+        0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40, 0xB401, 0x74C0, 0x7580, 0xB541,
+        0x7700, 0xB7C1, 0xB681, 0x7640, 0x7200, 0xB2C1, 0xB381, 0x7340, 0xB101,
+        0x71C0, 0x7080, 0xB041, 0x5000, 0x90C1, 0x9181, 0x5140, 0x9301, 0x53C0,
+        0x5280, 0x9241, 0x9601, 0x56C0, 0x5780, 0x9741, 0x5500, 0x95C1, 0x9481,
+        0x5440, 0x9C01, 0x5CC0, 0x5D80, 0x9D41, 0x5F00, 0x9FC1, 0x9E81, 0x5E40,
+        0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901, 0x59C0, 0x5880, 0x9841, 0x8801,
+        0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1, 0x8A81, 0x4A40, 0x4E00, 0x8EC1,
+        0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80, 0x8C41, 0x4400, 0x84C1, 0x8581,
+        0x4540, 0x8701, 0x47C0, 0x4680, 0x8641, 0x8201, 0x42C0, 0x4380, 0x8341,
+        0x4100, 0x81C1, 0x8081, 0x4040};
+    return ((crc & 0xFF00) >> 8) ^ tbl[(crc & 0x00FF) ^ (data & 0x00FF)];
+  }
 
-    bool isValidCRC(const unsigned char* buffer, int length) {
-        if (length < 3) return false;
-        return calculateCRC(buffer, length - 2) == buffer[length - 2] | (buffer[length - 1] << 8);
-    }
+  short calculateCRC(const unsigned char *commands, const int size) {
+    short crc = 0;
+    for (int i = 0; i < size; i++)
+      crc = CRC16(crc, commands[i]);
+    return crc;
+  }
 
-    void printHex(const vector<uint8_t>& data) {
-        for (uint8_t byte : data) printf("%02X ", byte);
-        printf("\n");
-    }
+  bool isValidCRC(const unsigned char *buffer, int length) {
+    if (length < 3)
+      return false;
+    return calculateCRC(buffer, length - 2) == buffer[length - 2] |
+           (buffer[length - 1] << 8);
+  }
 
+  void printHex(const vector<uint8_t> &data) {
+    for (uint8_t byte : data)
+      printf("%02X ", byte);
+    printf("\n");
+  }
 };
 
 int main() {
-    Uart uart("/dev/serial0", B115200);
-    uart.requestRead(Uart::SubCode::MOVE_X_LEFT_RIGHT);
-    uart.requestRead(Uart::SubCode::MOVE_Y_UP_DOWN);
-    uart.requestRead(Uart::SubCode::PRESET_POSITIONS);
-    uart.requestRead(Uart::SubCode::SET_PRESET_POSITION);
-    uart.requestRead(Uart::SubCode::CALIBRATE);
-    return 0;
+  Uart uart("/dev/serial0", B115200);
+  uart.requestRead(Uart::SubCode::MOVE_X_LEFT_RIGHT);
+  uart.requestRead(Uart::SubCode::MOVE_Y_UP_DOWN);
+  uart.requestRead(Uart::SubCode::PRESET_POSITIONS);
+  uart.requestRead(Uart::SubCode::SET_PRESET_POSITION);
+  uart.requestRead(Uart::SubCode::CALIBRATE);
+  return 0;
 }
