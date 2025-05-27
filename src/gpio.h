297b9af925fbde788dc1d4385213de902097aade
Hunter104 <000plagueinc@gmail.com>
2025-05-27 17:13:40 -0300

    Transforma defines em constexpr

--- src/gpio.h
+++ src/gpio.h
@@ -1,30 +1,28 @@
-#ifndef GPIO_H
-#define GPIO_H
+#pragma once
+#include <array>
 
-#define MOTOR_X_PWM 17
-#define MOTOR_X_DIR1 27
-#define MOTOR_X_DIR2 22
-#define MOTOR_Y_PWM 23
-#define MOTOR_Y_DIR1 24
-#define MOTOR_Y_DIR2 25
+constexpr int MOTOR_X_PWM = 17;
+constexpr int MOTOR_X_DIR1 = 27;
+constexpr int MOTOR_X_DIR2 = 22;
+constexpr int MOTOR_Y_PWM = 23;
+constexpr int MOTOR_Y_DIR1 = 24;
+constexpr int MOTOR_Y_DIR2 = 25;
 
-#define ENCODER_X_A 5
-#define ENCODER_X_B 6
-#define ENCODER_Y_A 12
-#define ENCODER_Y_B 13
+constexpr int ENCODER_X_A = 5;
+constexpr int ENCODER_X_B = 6;
+constexpr int ENCODER_Y_A = 12;
+constexpr int ENCODER_Y_B = 13;
 
-#define SENSOR_X_MIN 26
-#define SENSOR_X_MAX 19
-#define SENSOR_Y_MIN 20
-#define SENSOR_Y_MAX 21
+constexpr int SENSOR_X_MIN = 26;
+constexpr int SENSOR_X_MAX = 19;
+constexpr int SENSOR_Y_MIN = 20;
+constexpr int SENSOR_Y_MAX = 21;
 
-#define CAPTURA 18
+constexpr int CAPTURA = 18;
 
-#define BOTAO_CIMA 16
-#define BOTAO_BAIXO 1
-#define BOTAO_ESQ 6
-#define BOTAO_DIR 8
+constexpr int BOTAO_CIMA = 16;
+constexpr int BOTAO_BAIXO = 1;
+constexpr int BOTAO_ESQ = 6;
+constexpr int BOTAO_DIR = 8;
 
-#define BOTAO_EMERGENCIA 11
-
-#endif //GPIO_H
+constexpr int BOTAO_EMERGENCIA = 11;
