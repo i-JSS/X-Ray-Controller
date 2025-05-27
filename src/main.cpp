3e62f362f875243287e51046974c38254adcfd0c
Hunter104 <000plagueinc@gmail.com>
2025-05-27 16:30:58 -0300

    Consolida função main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
+#include "bmp280Controller.h"
 #include "modbusController.h"
 #include <csignal>
 #include <iostream>
+#include <ostream>
 
 ModbusController modbus("/dev/serial0", B115200);
+bmp280Controller bmp280;
+
 void handler(int) {
   modbus.ensureClosed();
+  bmp280.close();
   exit(0);
+}
+
+std::ostream &operator<<(ostream &os, const ModbusController::RegisterState &state) {
+  os << "Movendo:    LEFT=" << state.isMoving[0]
+     << " RIGHT=" << state.isMoving[1]
+     << " UP=" << state.isMoving[2]
+     << " DOWN=" << state.isMoving[3] << "\n";
+
+  os << "Preset:     P1=" << state.readingPreset[0]
+     << " P2=" << state.readingPreset[1]
+     << " P3=" << state.readingPreset[2]
+     << " P4=" << state.readingPreset[3]
+     << " | Configurando?=" << state.isSettingPreset << "\n";
+
+  os << "Calibrando?: " << state.isCalibrating << "\n";
+
+  return os;
 }
 
 int main() {
@@ -15,24 +37,14 @@ int main() {
   sigaction(SIGINT, &sa, nullptr);
 
   while (true) {
-    auto state = modbus.readRegisters();
+    auto screenState = modbus.readRegisters();
+    auto sensorState = bmp280.readData();
+    std::cout << screenState;
+    std::cout << "Temperature: " << sensorState.temperature << "ºC\n";
+    std::cout << "Pressure: " << sensorState.pressure << "hPA\n";
 
-    std::cout << "\033[3A";
-    std::cout << "\033[2K\r";
-    std::cout << "Movendo:    LEFT=" << state.isMoving[0]
-              << " RIGHT=" << state.isMoving[1]
-              << " UP=" << state.isMoving[2]
-              << " DOWN=" << state.isMoving[3] << "\n";
-
-    std::cout << "\033[2K\r";
-    std::cout << "Preset:     P1=" << state.readingPreset[0]
-              << " P2=" << state.readingPreset[1]
-              << " P3=" << state.readingPreset[2]
-              << " P4=" << state.readingPreset[3]
-              << " | Setando=" << state.isSettingPreset << "\n";
-
-    std::cout << "\033[2K\r";
-    std::cout << "Calibrando: " << state.isCalibrating << "\n";
+    modbus.write(ModbusController::SubCode::REG_TEMPERATURE, sensorState.temperature);
+    modbus.write(ModbusController::SubCode::REG_PRESSURE, sensorState.pressure);
   }
 
   return 0;
