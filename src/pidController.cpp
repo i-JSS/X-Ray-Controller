244cda32570d7ee8c3bc4ad0813bbb1c2cd4495f
Hunter104 <000plagueinc@gmail.com>
2025-05-30 12:01:01 -0300

    Adiciona classe de controle de pid

---
+++ src/pidController.cpp
@@ -0,0 +1,32 @@
+#include "pid.h"
+
+double PIDController::getControlSignal(double measuredOutput) {
+
+  double error = reference - measuredOutput;
+
+  accumulatedError += error;
+
+  if (accumulatedError >= MAX_CONTROL_SIGNAL) {
+    accumulatedError = MAX_CONTROL_SIGNAL;
+  } else if (accumulatedError <= MIN_CONTROL_SIGNAL) {
+    accumulatedError = MIN_CONTROL_SIGNAL;
+  }
+
+  double deltaError = error - prevError;
+
+  double controlSignal = Kp * error + (Ki * samplePeriod) * accumulatedError + (Kd / samplePeriod) * deltaError;
+
+  if (controlSignal >= MAX_CONTROL_SIGNAL) {
+    controlSignal = MAX_CONTROL_SIGNAL;
+  } else if (controlSignal <= MIN_CONTROL_SIGNAL) {
+    controlSignal = MIN_CONTROL_SIGNAL;
+  }
+
+  prevError = error;
+
+  return controlSignal;
+}
+
+void PIDController::setReference(double newReference) {
+  reference = newReference;
+}
